0279b615155ee803041f7526c66cac63bbf75251
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:48:41 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,7 +1,7 @@
-favorite_word = "doggie"
+your_favorite_word = "doggie"
 
 
-puts favorite_word.upcase
+puts your_favorite_word.upcase
 puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
 puts "wow" + "mom"
