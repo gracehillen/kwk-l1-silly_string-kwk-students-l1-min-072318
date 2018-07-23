d729bbca7c765f22b13aacc00df5dcf6e6ee9182
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:57:46 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -14,4 +14,5 @@ supercalifragilisticexpialidocious.length
 
 puts "wow" + "mom"
 puts "i really like programming".capitalize
+
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub ('A','O').gsub('a','o')
