a8975012e2e04f8790b467be551bc3f7fd1fe852
gracehillen <grahillen19@ahastars.org>
2018-07-23 20:56:55 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -6,9 +6,12 @@ favorite_word_in_all_caps = your_favorite_word.upcase
 puts "My favorite word is #{favorite_word_in_all_caps}"
 
 phrase = "IM NOT SHOUTING"
-puts "IM NOT SHOUTING".downcase
 
-puts "supercalifragilisticexpialidocious".length
+lowered_phrase = phrase.downcase
+puts lowered_phrase
+
+supercalifragilisticexpialidocious.length 
+
 puts "wow" + "mom"
 puts "i really like programming".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub ('A','O').gsub('a','o')
